622c53a63ebc43a4ce33fd917cafaef8e178c84d
vekkt <francois.burguet@epfl.ch>
2021-04-18 11:57:41 +0200

    fixed __init__

--- project2/sequential.py
+++ project2/sequential.py
@@ -2,9 +2,10 @@ from module import Module
 
 class Sequential(Module):
     def __init__(self, *args):
+        super(Sequential, self).__init__()
         for module in args:
             self._modules.append(module)
-            for p in module.param():
+            for p in module.parameters():
                 self._parameters.append(p)
     
     def _activation_function(self, input):
