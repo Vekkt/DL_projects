1ac247b8cb1df73a8690061a16237bfb1e8f33b1
vekkt <francois.burguet@epfl.ch>
2021-04-18 16:53:54 +0200

    fixed typos, changed init

--- project2/sgd.py
+++ project2/sgd.py
@@ -1,8 +1,8 @@
 class SGD():
-    def __init__(self, params, lr=1e-1):
-        self._parameters = params
+    def __init__(self, model, lr=1e-1):
+        self._model = model
         self._lr = lr
 
     def step(self):
-        for p, grad in self._parameters:
-            p._add(grad, alpha=-self._lr)
+        for p, grad in self._model.parameters():
+            p.add_(grad, alpha=-self._lr)
