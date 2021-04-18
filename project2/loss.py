e3c5a49c9d01b38ed17c8f5772990d33a1475084
vekkt <francois.burguet@epfl.ch>
2021-04-18 11:57:20 +0200

    completed implementation

--- project2/loss.py
+++ project2/loss.py
@@ -1,13 +1,14 @@
 from module import Module
+import functional as F
 
 class MSELoss(Module):
     def forward(self, input, target):
         self._input = input
         self._target = target
-        return 
+        return F.mse(input, target)
 
     def backward(self, input, target):
-        return 
+        return F.dmse(input, target)
 
     def __call__(self, input, target):
         return self.forward(input, target)
