ea0b977330df72af87a980d4ff0f7cc55684c318
vekkt <francois.burguet@epfl.ch>
2021-04-18 16:54:42 +0200

    changed init to accept model

--- project2/loss.py
+++ project2/loss.py
@@ -3,13 +3,16 @@ import functional as F
 from torch import set_grad_enabled
 
 class MSELoss(Module):
+    def __init__(self, model):
+        self._model = model
+
     def forward(self, input, target):
         self._input = input
         self._target = target
         return F.mse(input, target)
 
     def backward(self):
-        return F.dmse(self._input, self._target)
+        return self._model.backward(F.dmse(self._input, self._target))
 
     def __call__(self, input, target):
         return self.forward(input, target)
