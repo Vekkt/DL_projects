9fbf94400421dd9a4b2c9203f1eb28d62ac1a68f
vekkt <francois.burguet@epfl.ch>
2021-04-24 23:05:16 +0200

    updated cross entropy

--- project2/functional.py
+++ project2/functional.py
@@ -24,11 +24,11 @@ def dmse(x, y):
     return (x-y).mul(2).div(x.numel())
 
 def cross_entropy(x, y):
-    p = x.new_zeros(y.size(0), x.shape[1]).scatter(1, y.view(-1, 1), 1)
-    q = x.exp().div(x.exp().sum(axis=1).view(-1, 1).repeat(1, x.shape[1]))
+    p = x.new_zeros(y.size(0), x.size(1)).scatter(1, y.view(-1, 1), 1)
+    q = x.exp().div(x.exp().sum(axis=1).view(-1, 1).repeat(1, x.size(1)))
     return (p.mul(q.log())).mean(axis=0).sum().mul(-1)
 
 def dcross_entropy(x, y):
-    p = x.new_zeros(y.size(0), x.shape[1]).scatter(1, y.view(-1, 1), 1)
-    q = x.exp().div(x.exp().sum(axis=1).view(-1, 1).repeat(1, x.shape[1]))
+    p = x.new_zeros(y.size(0), x.size(1)).scatter(1, y.view(-1, 1), 1)
+    q = x.exp().div(x.exp().sum(axis=1).view(-1, 1).repeat(1, x.size(1)))
     return q.sub(p)
