251b6b2b53e947c4a5c4018fa09c759b65b6aef8
vekkt <francois.burguet@epfl.ch>
2021-04-24 23:01:23 +0200

    implemented Loss class

--- project2/loss.py
+++ project2/loss.py
@@ -1,18 +1,20 @@
 from module import Module
 import functional as F
 
-class MSELoss(Module):
-    def __init__(self, model):
+class Loss(Module):
+    def __init__(self, model, foward_fun, backward_fun):
         self._model = model
+        self._forward = foward_fun
+        self._backward = backward_fun
 
     def forward(self, input, target):
         self._input = input
         self._target = target
-        self._loss = F.mse(input, target)
+        self._loss = self._forward(input, target)
         return self
 
     def backward(self):
-        return self._model.backward(F.dmse(self._input, self._target))
+        return self._model.backward(self._backward(self._input, self._target))
 
     def __call__(self, input, target):
         return self.forward(input, target)
@@ -20,22 +22,14 @@ class MSELoss(Module):
     def __add__(self, other):
         return other + self._loss
 
-
-class CrossEntropyLoss(Module):
+class MSELoss(Loss):
     def __init__(self, model):
-        self._model = model
+        forward_fun = F.mse
+        backward_fun = F.dmse
+        super(MSELoss, self).__init__(model, forward_fun, backward_fun)
 
-    def forward(self, input, target):
-        self._input = input
-        self._target = target
-        self._loss = F.cross_entropy(input, target)
-        return self
-
-    def backward(self):
-        return self._model.backward(F.dcross_entropy(self._input, self._target))
-
-    def __call__(self, input, target):
-        return self.forward(input, target)
-
-    def __add__(self, other):
-        return other + self._loss
+class CrossEntropyLoss(Loss):
+    def __init__(self, model):
+        forward_fun = F.cross_entropy
+        backward_fun = F.dcross_entropy
+        super(CrossEntropyLoss, self).__init__(model, forward_fun, backward_fun)
