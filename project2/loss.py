51411c51ae4cebd62aaf05670ea639636fe812ea
vekkt <francois.burguet@epfl.ch>
2021-04-24 20:00:46 +0200

    added cross-entropy loss

--- project2/loss.py
+++ project2/loss.py
@@ -21,3 +21,22 @@ class MSELoss(Module):
     def __add__(self, other):
         return other + self._loss
 
+
+class CrossEntropyLoss(Module):
+    def __init__(self, model):
+        self._model = model
+
+    def forward(self, input, target):
+        self._input = input
+        self._target = target
+        self._loss = F.cross_entropy(input, target)
+        return self
+
+    def backward(self):
+        return self._model.backward(F.dcross_entropy(self._input, self._target))
+
+    def __call__(self, input, target):
+        return self.forward(input, target)
+
+    def __add__(self, other):
+        return other + self._loss
