a9008aa1147b699b3f55ea5cf5b3d7c0e129d3aa
vekkt <francois.burguet@epfl.ch>
2021-04-25 12:09:35 +0200

    implemented momentum

--- project2/sgd.py
+++ project2/sgd.py
@@ -1,13 +1,19 @@
+from torch import empty
+
 ''' Stochastic gradient descent algorithm representative 
     class. Given a model and a learning rate, it allows
     to update the parameters of the model according to
     the gradient values for each parameter.
 '''
 class SGD():
-    def __init__(self, params, lr=1e-1):
+    def __init__(self, params, lr=1e-1, momentum=0):
         assert(lr>0)
         self._parameters = params
-        self._lr = lr
+        self.lr = lr
+        self.momentum = momentum
+        self.momentum_buffer = []
+        for _, grad in self._parameters:
+            self.momentum_buffer.append(empty(grad.size()).zero_())
 
     ''' Makes one step for the gradient descent algorithm
         For each parameter, update its value according to 
@@ -18,5 +24,6 @@ class SGD():
         values update everywhere.
     '''
     def step(self):
-        for p, grad in self._parameters:
-            p.add_(grad, alpha=-self._lr)
+        for (p, grad), m in zip(self._parameters, self.momentum_buffer):
+            m.mul_(self.momentum).add_(grad, alpha=self.lr)
+            p.add_(m, alpha=-1.)
