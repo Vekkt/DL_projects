cd83eca96b8bd1b93a14c8da5bd770e32409a723
Vekkt <francois.burguet@epfl.ch>
2021-05-09 18:25:42 +0200

    Update init.py

--- project2/init.py
+++ project2/init.py
@@ -1,20 +1,11 @@
 from math import sqrt
 
-def _calculate_fan_in_and_fan_out(tensor):
-    num_input_fmaps = tensor.size(1)
-    num_output_fmaps = tensor.size(0)
-    receptive_field_size = 1
-    
-    if tensor.dim() > 2:
-        receptive_field_size = tensor[0][0].numel()
-        
-    fan_in = num_input_fmaps * receptive_field_size
-    fan_out = num_output_fmaps * receptive_field_size
-
-    return fan_in, fan_out
-
 def xavier_normal_(tensor, gain=1.):
-    fan_in, fan_out = _calculate_fan_in_and_fan_out(tensor)
-    std = gain * sqrt(2.0 / float(fan_in + fan_out))
+    if tensor.dim() > 2:
+        raise ValueError("Unsupported tensor format")
+        
+    n_in = tensor.size(1)
+    n_out = tensor.size(0)
+    std = gain * sqrt(6.0 / float(n_in + n_out))
 
     return tensor.normal_(0., std)
