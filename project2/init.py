b5917691f3cbfb006f75bfef3e7696584aba824c
Vekkt <francois.burguet@epfl.ch>
2021-05-09 16:30:50 +0200

    Update init.py

--- project2/init.py
+++ project2/init.py
@@ -1,16 +1,13 @@
 from math import sqrt
 
 def _calculate_fan_in_and_fan_out(tensor):
-    dimensions = tensor.dim()
-    if dimensions < 2:
-        raise ValueError(
-            "Fan in and fan out can not be computed for tensor with fewer than 2 dimensions")
-
     num_input_fmaps = tensor.size(1)
     num_output_fmaps = tensor.size(0)
     receptive_field_size = 1
+    
     if tensor.dim() > 2:
         receptive_field_size = tensor[0][0].numel()
+        
     fan_in = num_input_fmaps * receptive_field_size
     fan_out = num_output_fmaps * receptive_field_size
 
