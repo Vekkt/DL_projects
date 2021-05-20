dec5297ad904f41a33f9e3482ca9ace911137281
vekkt <francois.burguet@epfl.ch>
2021-05-20 19:01:56 +0200

    added data set creation

--- project1/main.py
+++ project1/main.py
@@ -5,5 +5,6 @@ from convNet import *
 data = generate_pair_sets(100)
 train_input, train_target, train_classes = data[:3]
 test_input, test_target, test_classes = data[3:]
+
 model = PairNet(aux_loss=True)
 
